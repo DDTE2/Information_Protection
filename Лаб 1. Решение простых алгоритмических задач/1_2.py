98e48019546f640b9c517262195f5251b9b658f8
DDTE2 <sshagotskiy@list.ru>
2023-01-17 23:28:08 +1000

    Вторая редакция

--- Лаб 1. Решение простых алгоритмических задач/1_2.py
+++ Лаб 1. Решение простых алгоритмических задач/1_2.py
@@ -4,18 +4,18 @@ from os.path import relpath
 from json import dumps
 
 path = input('Директория:\n')
-name = input('Название файла:\n')
+name = input('Расширение файла:\n')
 
 res = {}
 for root,dir,files in walk(path):
     for file in files:
-        if file == name:
+        if file.endswith(name):
             if root in res:
                 res[root].append(file)
             else:
                 res[root] = [file]
 
-with open('Результаты/' + relpath(__file__)[:-3] + '.txt', 'w', encoding='windows-1251') as save:
-    save.write(dumps(res, sort_keys=True, indent=4))
+with open('Результаты/' + relpath(__file__)[:-3] + '.json', 'w', encoding='windows-1251') as save:
+    save.write(dumps(res, sort_keys=True, indent=4, ensure_ascii=False))
 
 
