026bb859603a878d6592ebeeed32f5b01bd0b3e4
DDTE2 <sshagotskiy@list.ru>
2023-01-21 16:02:53 +1000

    1_12 (неудачный)

--- Лаб 1. Решение простых алгоритмических задач/1_12.py
+++ Лаб 1. Решение простых алгоритмических задач/1_12.py
@@ -1,61 +1,46 @@
 from json import dumps
+from data_save import data_save
+from os.path import relpath
 
 path = input("Путь к файлу:\n")
 path += "\\" + input("Название файла:\n")
 print(path)
 separator = input("Разделитель логина и пароля:\n")
 
-def data_read():
-    global path, separator
-    passwords = {}
+passw = {}
+for codec in ('utf-32', "utf-32be", "cp1252", "cp437", 'windows-1251', 'ascii'):
+    print(codec, end=';')
+    try:
+        with open(path, 'r', encoding=codec) as file:
+            for a in file.readlines():
+                login, *password = a[:-1].split(':')
+                password = ':'.join(password)
 
-    for enc in ('utf-32', "utf-32be", "cp1252", "cp437", 'windows-1251', 'ascii'):
+                if not password in passw:
+                    passw[password] += 1
+                else:
+                    passw[password] = 1
+
+        break
+    except Exception as error:
+        print(error)
+
+if passw:
+    for codec in ('utf-32', "utf-32be", "cp1252", "cp437", 'windows-1251', 'ascii'):
+        print(codec, end=';')
         try:
-            with open(path, 'r', encoding=enc) as file:
-                for line in file.read().split():
-                    login, password = line.split(separator)
-                    if password in passwords:
-                        passwords[password] += 1
+            with open(path, 'rb', encoding=codec) as file:
+                for a in file.readlines():
+                    login, *password = a.split(':')
+                    password = ':'.join(password)
+
+                    if not password in passw:
+                        passw[password] += 1
                     else:
-                        passwords[password] = 1
-            return passwords
+                        passw[password] = 1
+
+            break
         except Exception as error:
-            print(error, enc)
+            print(error)
 
-    if not passwords:
-        for enc in ('utf-32', "utf-32be", "cp1252", "cp437", 'windows-1251', 'ascii'):
-            try:
-                with open(path, 'rb', encoding=enc) as file:
-                    for line in file.read().split():
-                        login, password = line.split(separator)
-                        if password in passwords:
-                            passwords[password] += 1
-                        else:
-                            passwords[password] = 1
-                return passwords
-            except Exception as error:
-                print(error, enc)
-
-def data_save():
-    global path, separator
-    passwords = data_read
-
-    if passwords:
-        data = {}
-        for password in passwords:
-            count = passwords[password]
-            if count in data:
-                data[count].add(password)
-            else:
-                data[count] = {password}
-
-        data = {count:sorted(data[count]) for count in data}
-
-        passwords.clear()
-        res = ''
-        for count in sorted(data):
-            for password in data[count]:
-                res += f'{password}: {count}\n'
-
-        data.clear()
-        print(res)
+print(passw)
